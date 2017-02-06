76d3c4562eb0559c1f199d908ae4efd303f5584a
hecha <nt_d_hecha@wa3.so-net.ne.jp>
2017-02-07 01:07:47 +0900

    設定用スクリプト。

---
+++ dotfiles_setup.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+ln -sf ~/dotfiles/vim/.vim/colors ~/.vim
+ln -sf ~/dotfiles/vim/.vim/bundle ~/.vim
+ln -sf ~/dotfiles/vim/.vim/.netrwhist ~/.vim
+ln -sf ~/dotfiles/vim/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/VScode/Code/User ~/Library/Application\ Support/Code/User
